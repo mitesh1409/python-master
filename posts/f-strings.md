271cf163bf7e0501d4e24a33dc4458b00f9c3211
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-08-20 10:17:23 +0530

    🚀

---
+++ posts/f-strings.md
@@ -0,0 +1,20 @@
+# f-strings
+
+f-strings, short for "formatted string literals".  
+
+An f-string lets you build a string  
+without having to rely on the  
+troublesome + operator. Instead, you  
+embed your variable values into the  
+f-string.  
+
+**Variable Interpolation**  
+When f-string sees variable names enclosed in curly braces `{}`,  
+it gets the value of variables and inserts it into the place where  
+variable names are enclosed in curly braces `{}`.
+
+In other words we can embed variables into a string using curly braces `{}`,  
+and they will be replaced by their values via **Variable Interpolation** mechanism.  
+
+Reference:  
+https://docs.python.org/3/reference/lexical_analysis.html#f-strings
--- projects/swim-club/swimclub.py
+++ projects/swim-club/swimclub.py
@@ -1,9 +1,13 @@
 import statistics
+import hfpy_utils
 
-__all__ = ['process_swim_data']
+__all__ = ["process_swim_data", "generate_bar_chart"]
 
 # File location
-FOLDER = "swimdata"
+DATA_FOLDER = "swimdata"
+
+# Charts location
+CHARTS_FOLDER = "charts"
 
 def _time_to_hundredths_seconds(time_str):
     parts = time_str.split(":")
@@ -24,18 +28,18 @@ def _time_to_hundredths_seconds(time_str):
     return total_hundredths
 
 def _time_to_minutes_seconds_hundredths(time_value):
-    mins_secs, hundredths = str(round(time_value / 100, 2)).split(".")
+    mins_secs, hundredths = f"{(time_value / 100):.2f}".split(".")
     mins_secs = int(mins_secs)
     mins = mins_secs // 60
     secs = mins_secs - (mins * 60)
-    return f'{mins}:{secs}.{hundredths}'
+    return f'{mins}:{secs:0>2}.{hundredths}'
 
 def process_swim_data(filename):
     # Extracting data from the filename.
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
 
     # Reading data from the file
-    with open(f"{FOLDER}/{filename}") as file:
+    with open(f"{DATA_FOLDER}/{filename}") as file:
         lines = file.readlines()
 
     # We have only one line of data, getting time entries from it.
@@ -48,4 +52,58 @@ def process_swim_data(filename):
 
     average_time = _time_to_minutes_seconds_hundredths(statistics.mean(time_values))
 
-    return swimmer, age, distance, stroke, times, average_time
+    return swimmer, age, distance, stroke, times, average_time, time_values
+
+def generate_bar_chart(filename):
+    # Process file data
+    swimmer, age, distance, stroke, times, average, time_values = process_swim_data(filename)
+
+    # Construct the title
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+
+    # Construct SVG bars
+    max_time_value = max(time_values)
+    svg_bars = ""
+
+    times.reverse()
+    time_values.reverse()
+    for n, t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(time_values[n], 0, max_time_value, 0, 400)
+
+        svg_bar = f"""
+        <svg height="30" width="450">
+            <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+        </svg>{t}<br />
+        """
+
+        svg_bars += svg_bar
+
+    # File to save bar charts
+    save_to = f"charts/{filename.replace(".txt", ".html")}"
+
+    # HTML + SVG content for the bar chart file
+    html_page = f"""
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>
+            {title}
+        </title>
+    </head>
+
+    <body>
+        <h3>{title}</h3>
+
+        {svg_bars}
+
+        <p>Average time: {average}</p>
+    </body>
+
+    </html>
+    """
+
+    with open(save_to, "w") as tf:
+        print(html_page, file=tf)
+
+    return save_to
