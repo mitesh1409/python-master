6165906c64d806643c0bfc7d80e5ee0824c78c18
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-08-24 18:53:08 +0530

    🚀

--- projects/swim-club-webapp/app.py
+++ projects/swim-club-webapp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, session, render_template, request
-import os
 
 import swimclub
+import hfpy_utils
 
 app = Flask(__name__)
 app.secret_key = "ABCD-5678-WXYZ"
@@ -14,7 +14,7 @@ def load_session():
 def index():
     return render_template(
         "index.html",
-        title="Welcome to the Swimclub system"
+        title="Welcome to Swimclub"
     )
 
 @app.get("/swimmers")
@@ -22,30 +22,48 @@ def display_swimmers():
     load_session()
     swimmers = sorted(list(session["swimmers"].keys()))
     return render_template(
-        "select.html",
+        "swimmers.html",
         title="Select a swimmer",
         select_id="swimmer",
-        data=swimmers,
-        url="/showfiles"
+        swimmers=swimmers,
+        url="/events"
     )
 
-@app.post("/showfiles")
-def display_swimmers_files():
+@app.post("/events")
+def display_events():
     load_session()
     name = request.form["swimmer"]
     return render_template(
-        "select.html",
+        "events.html",
         title="Select an event",
-        select_id="file",
-        data=session["swimmers"][name],
-        url="/showbarchart"
+        select_id="event",
+        files=session["swimmers"][name],
+        url="/charts"
     )
 
-@app.post("/showbarchart")
-def show_bar_chart():
-    file_id = request.form["file"]
-    location = swimclub.generate_bar_chart(file_id, "templates")
-    return render_template(location.split("/")[1])
+@app.post("/charts")
+def display_charts():
+    file_id = request.form["event"]
+    swimmer, age, distance, stroke, times, average, time_values = swimclub.process_swim_data(file_id)
+
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+
+    max_time_value = max(time_values)
+    bar_values = []
+    times.reverse()
+    time_values.reverse()
+    for n, time in enumerate(times):
+        bar_values.append((
+            hfpy_utils.convert2range(time_values[n], 0, max_time_value, 0, 400),
+            time
+        ))
+
+    return render_template(
+        "barchart.html",
+        title=title,
+        average=average,
+        bar_values=bar_values
+    )
 
 if __name__ == "__main__":
     app.run(debug=False)
